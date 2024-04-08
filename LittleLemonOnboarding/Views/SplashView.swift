20218e13ca7ffdc7258ad5682838fe7f405ff6b6
flngsquirrl <y.charniak@gmail.com>
2024-04-08 14:27:21 +0200

    Rename image asset for splash screen

--- LittleLemonOnboarding/Views/SplashView.swift
+++ LittleLemonOnboarding/Views/SplashView.swift
@@ -14,7 +14,7 @@ struct SplashView: View {
 
     var body: some View {
         if isLoading {
-            Image("Logo")
+            Image("splashLogo")
                 .resizable()
                 .scaledToFit()
                 .onAppear() {
